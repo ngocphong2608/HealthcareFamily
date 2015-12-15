6f71f8b35e9026e4a529536c2c07f2460531ed72
Thanh Phong <tranthanhphong260895@gmail.com>
2015-12-15 23:53:32 +0700

    version 1.0.9

---
+++ HealthcareFamily/HeathcareFamilyBUS/UserImformationBUS.cs
@@ -0,0 +1,19 @@
+﻿using HealthcareFamilyDTO;
+using HeathcareFamilyDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeathcareFamilyBUS
+{
+    public class UserImformationBUS
+    {
+        public UserInformation LoadUserInformation(String username)
+        { 
+            UserInformationDAL user = new UserInformationDAL();
+            return user.LoadUserInformation(username);
+        }
+    }
+}
